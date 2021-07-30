ce1449b0da056cf6dd95eb46b2e73da9742b27d3
sushilsaladi <saladi.sushil@gmail.com>
2021-07-30 13:52:07 -0400

    First run

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//rcfe
+function Header() {
+    return (
+        <nav className="header">
+            {/* Logo on the left -> img */}
+
+            {/* Search box */}
+            {/* 3 links */}
+            {/* Basket Icon with number*/}
+
+
+        </nav>
+    )
+}
+
+export default Header
